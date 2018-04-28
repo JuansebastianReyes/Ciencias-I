783745ae84003136dc8737acbdf70aa019accecf
JuansebastianReyes <reyes_garcia1103@hotmail.com>
2018-04-27 22:37:29 -0500

    Se mejora el proyecto BM y se agrega el modo espejo

--- Algoritmo BM/Bm-Go/main.go
+++ Algoritmo BM/Bm-Go/main.go
@@ -4,6 +4,7 @@ import (
 		"io/ioutil"
 		"fmt"
 		"unicode/utf8"
+		"strings"
 		)
 
 func main() {
@@ -14,18 +15,28 @@ func main() {
      fmt.Println("Error en el archivo")
    }
 
-   fmt.Println(string(fileData))
 
    var i int
    haystack := string(fileData)
-   needle := "hermano"
+   needle := "el"
+   i = Search(haystack, needle)
+
+   fmt.Println("ALGORITMO DE BOYER MOORE")
+   fmt.Println("___________________________")
+   fmt.Println("Su texto es: ")
+   fmt.Println("___________________________")
+   fmt.Println(string(fileData))
+   fmt.Println("___________________________")
+   fmt.Println("El texto en esperjo es: ")
+   fmt.Println("___________________________")
+   fmt.Println(Divide(haystack))
+   fmt.Println("___________________________")
 
    i = Search(haystack, needle)
 
    fmt.Printf("El patron -> %s\n",needle)
    fmt.Printf("Esta %d\n",i)
-   fmt.Printf("veces en el texto -> \n")
-   fmt.Printf(haystack)
+   fmt.Printf("veces en el texto")
 
 }
 
@@ -43,8 +54,7 @@ func BuildSkipTable(needle string) map[rune]int {
 	return table
 }
 
-// search a needle in haystack and return count of needle.
-// table is build by BuildSkipTable.
+
 func SearchBySkipTable(haystack, needle string, table map[rune]int) int {
 
 	i, c := 0, 0
@@ -52,6 +62,7 @@ func SearchBySkipTable(haystack, needle string, table map[rune]int) int {
 	nrunes := []rune(needle)
 	hl := utf8.RuneCountInString(haystack)
 	nl := utf8.RuneCountInString(needle)
+	var pasos[] int
 
 	if hl == 0 || nl == 0 || hl < nl {
 		return 0
@@ -68,6 +79,7 @@ loop:
 				if _, ok := table[hrunes[i+j]]; !ok {
 					if j == nl-1 {
 						i += nl
+						
 					} else {
 						i += nl - j - 1
 					}
@@ -85,18 +97,60 @@ loop:
 
 		if _, ok := table[hrunes[i+nl-1]]; ok {
 			i += table[hrunes[i+nl-1]]
+			pasos = append(pasos,i+2)
 		} else {
 			i += nl
+			pasos = append(pasos,i)
 		}
+
+			
 
 		c++
 	}
 
+	fmt.Println(Underline(hrunes,pasos,nl))
+	fmt.Println("___________________________")
 	return c
 }
 
-// search a needle in haystack and return count of needle.
 func Search(haystack, needle string) int {
 	table := BuildSkipTable(needle)
 	return SearchBySkipTable(haystack, needle, table)
+}
+
+
+
+func Underline(hrunes []rune, pasos []int, nl int)string{
+
+	for i:=0;i<len(pasos);i++{
+		for j:=1;j<=int(nl);j++{
+			x:=pasos[i]-j
+			hrunes[x]=rune(45)
+		}
+	}
+	return string(hrunes)
+}
+
+func Reverse(s string) string{
+	
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+func Divide(s string) string{
+	var text[] string
+	var jtext[] string
+	text = strings.Split(s," ")
+	for i:=0;i<len(text);i++{
+		jtext=append(jtext,Reverse(text[i]))
+	}
+	return Join(jtext)
+}
+
+func Join(s []string) string{
+	text := strings.Join(s," ")
+	return text
 }
